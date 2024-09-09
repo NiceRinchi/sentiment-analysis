c8fae94ee4f9f55299fbecefb1198cf19ac83cb0
Alexander <166836193+NiceRinchi@users.noreply.github.com>
2024-09-09 15:34:24 +0300

    System-analysis 1.1

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 file_path = 'Path to file/file.csv'# Set the path to csv file to process it
 sentiment_pipeline = pipeline("sentiment-analysis")
 df = pd.read_csv(file_path)
-texts = df['review'].tolist()
+texts = df['Name of column to analyze'].tolist()
 results = sentiment_pipeline(texts)
 for i, result in enumerate(results):
-    print(f"Review {i+1}: {result['label']} (Confidence: {result['score']:.2f})")
+    print(f"Text {i+1}: {result['label']} (Confidence: {result['score']:.2f})")
